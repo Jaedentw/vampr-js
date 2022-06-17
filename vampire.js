1af4d1b142492af618874987349e7d94e2cba0e0
Jaeden West <jaeden.t.west@gmail.com>
2022-06-17 12:02:51 -0600

    implements all functions of vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
 
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const vampLine1 = vampire.numberOfVampiresFromOriginal;
+    const vampLine2 = this.numberOfVampiresFromOriginal
+    if(vampLine1 >= vampLine2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -41,4 +56,3 @@ class Vampire {
 }
 
 module.exports = Vampire;
-
